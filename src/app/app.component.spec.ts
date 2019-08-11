fa56fe82f0a546e2f75d8ba9d97bea07870582f1
Charles <ellerkaresz@gmail.com>
2019-08-11 18:55:12 +0200

    add search-results component; display fetched data

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'recipefinder'`, () => {
+  it(`should have as title 'Recipe Finder'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('recipefinder');
+    expect(app.title).toEqual('Recipe Finder');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to recipefinder!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Recipe Finder!');
   });
 });
---
+++ src/app/search-results/search-results.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-results',
+  templateUrl: './search-results.component.html',
+  styleUrls: ['./search-results.component.scss']
+})
+
+export class SearchResultsComponent {
+  recipes = [];
+
+  async fetchRecipe(id: number) {
+    const res = await fetch(`https://www.food2fork.com/api/get?key=c481f3a84bf7098d43a3bc7699a7567c&rId=${id}`);
+    const recipe = await res.json();
+
+    this.recipes.forEach(item => {
+      if (item.recipe_id === recipe.recipe.recipe_id) {
+        item.ingredients = recipe.recipe.ingredients;
+      }
+    });
+  }
+
+  async searchByKeywords(query: string, page: number = 1) {
+    const res = await fetch(`https://www.food2fork.com/api/search?key=c481f3a84bf7098d43a3bc7699a7567c&q=${query}&page=${page}`);
+    const results = await res.json();
+
+    this.recipes = results.recipes;
+  }
+}
